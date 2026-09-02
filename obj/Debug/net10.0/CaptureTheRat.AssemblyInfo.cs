c00c95f1a81d0a4696715a3c80654901f6034ab3
Kaitlynn <kaitlynnperkins101@gmail.com>
2026-09-02 13:53:34 -0400

    Requirements.txt

--- obj/Debug/net10.0/CaptureTheRat.AssemblyInfo.cs
+++ obj/Debug/net10.0/CaptureTheRat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaptureTheRat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0185b1307b7025a468f29c670427913c53a61a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaptureTheRat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaptureTheRat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
